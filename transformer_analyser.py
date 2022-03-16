f773601f0eac152a418f47b8174a933f9d6dc393
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2022-03-16 21:42:44 +0000

    Saving the models locally, and run them without going to the cloud.

--- transformer_analyser.py
+++ transformer_analyser.py
@@ -13,15 +13,15 @@ from transformers import AutoTokenizer, BertTokenizerFast, TFAutoModelForSequenc
 SEED: int = 0
 
 GROUP_IDENTITY_CLASS: str = "LABEL_1"
+TEXT_CONTENT_COLUMN = "text"
+TEXT_LABEL_COLUMN = "label"
+NON_ESSENTIAL_COLUMNS = ["title", "URL", "starting_time", "context", "logged_by", "Observation"]
 
 
 class TransformerTypeAnalyser(object):
 
     def __init__(self):
         self.model_checkpoint: str = "bert-base-uncased"
-        self.text_column: str = "text"
-        self.label_column: str = "will_help"
-        self.irrelevant_columns = ["tweet_id", "wont_help"]
         self.hub_model_id: str = "cptanalatriste/request-for-help"
         self.pipeline_task: str = "text-classification"
         self.output_directory: str = "./model"
@@ -29,13 +29,13 @@ class TransformerTypeAnalyser(object):
         self.num_labels: int = 2
         self.batch_size: int = 32
         self.learning_rate: float = 3e-5
-        self.epochs = 20
+        self.epochs = 65
 
         self.tokenizer: Optional[BertTokenizerFast] = None
         self.model: Optional[Model] = None
 
     def tokenize(self, data_batch):
-        return self.tokenizer(data_batch[self.text_column], truncation=True, max_length=128)
+        return self.tokenizer(data_batch[TEXT_CONTENT_COLUMN], truncation=True, max_length=128)
 
     @staticmethod
     def convert_csv_to_dataset(csv_file: str) -> Dataset:
@@ -47,20 +47,20 @@ class TransformerTypeAnalyser(object):
     def convert_dataset_to_tensorflow(self, dataset: Dataset, shuffle: bool) -> tf.data.Dataset:
         data_collator: DataCollatorWithPadding = DataCollatorWithPadding(tokenizer=self.tokenizer, return_tensors="tf")
         tensorflow_dataset: tf.data.Dataset = dataset.to_tf_dataset(columns=self.tokenizer.model_input_names,
-                                                                    label_cols=[self.label_column],
+                                                                    label_cols=[TEXT_LABEL_COLUMN],
                                                                     shuffle=shuffle,
                                                                     batch_size=self.batch_size,
                                                                     collate_fn=data_collator)
 
         return tensorflow_dataset
 
-    def train(self, training_data_file: str, testing_data_file: str):
+    def train(self, training_data_file: str, testing_data_file: str, local=False):
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_checkpoint)
         self.model = TFAutoModelForSequenceClassification.from_pretrained(self.model_checkpoint,
                                                                           num_labels=self.num_labels)
         self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate),
-                           loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                           metrics=tf.metrics.SparseCategoricalAccuracy())
+                           loss="binary_crossentropy",
+                           metrics=["accuracy"])
 
         dataset_dict: DatasetDict = DatasetDict({
             "train": self.convert_csv_to_dataset(training_data_file),
@@ -69,21 +69,27 @@ class TransformerTypeAnalyser(object):
 
         logging.info("Encoding content for tweets")
         encoded_datasets: DatasetDict = dataset_dict.map(self.tokenize, batched=True)
-        encoded_datasets = encoded_datasets.remove_columns(self.irrelevant_columns)
+        encoded_datasets = encoded_datasets.remove_columns(NON_ESSENTIAL_COLUMNS)
 
         training_dataset: tf.data.Dataset = self.convert_dataset_to_tensorflow(encoded_datasets["train"], shuffle=True)
         testing_dataset: tf.data.Dataset = self.convert_dataset_to_tensorflow(encoded_datasets["test"], shuffle=False)
 
         logging.info(f"Encoding finished!")
 
-        push_to_hub_callback: PushToHubCallback = PushToHubCallback(output_dir=self.output_directory,
-                                                                    tokenizer=self.tokenizer,
-                                                                    hub_model_id=self.hub_model_id)
+        if local:
+            self.model.fit(training_dataset, validation_data=testing_dataset, epochs=self.epochs)
+            self.model.save_pretrained(self.output_directory)
+        else:
+            push_to_hub_callback: PushToHubCallback = PushToHubCallback(output_dir=self.output_directory,
+                                                                        tokenizer=self.tokenizer,
+                                                                        hub_model_id=self.hub_model_id)
 
-        self.model.fit(training_dataset, validation_data=testing_dataset, epochs=self.epochs,
-                       callbacks=push_to_hub_callback)
+            self.model.fit(training_dataset, validation_data=testing_dataset, epochs=self.epochs,
+                           callbacks=push_to_hub_callback)
 
-        logging.info(f"Training finished! Model is available at the hub with id {self.hub_model_id}")
+            logging.info(f"Training finished! Model is available at the hub with id {self.hub_model_id}")
+
+        logging.info(f"Model saved at {self.output_directory}")
 
     def obtain_probabilities(self, text_as_string: str, local=False) -> float:
         classification_pipeline: Pipeline
@@ -91,7 +97,7 @@ class TransformerTypeAnalyser(object):
             logging.info(f"Loading model from {self.output_directory}")
             classification_pipeline = pipeline(task=self.pipeline_task,
                                                model=self.output_directory,
-                                               tokenizer=self.output_directory)
+                                               tokenizer=self.tokenizer)
         else:
             classification_pipeline = pipeline(task=self.pipeline_task, model=self.hub_model_id)
 
@@ -103,13 +109,13 @@ class TransformerTypeAnalyser(object):
         return group_identity_probability
 
 
-def start_training(training_csv: str, testing_csv_file: str) -> None:
+def start_training(training_csv: str, testing_csv_file: str, local: bool) -> None:
     logging.basicConfig(level=logging.INFO)
     logging.info(f"training_csv {training_csv}")
     logging.info(f"testing_csv_file {testing_csv_file}")
 
     type_analyser: TransformerTypeAnalyser = TransformerTypeAnalyser()
-    type_analyser.train(training_csv, testing_csv_file)
+    type_analyser.train(training_csv, testing_csv_file, local)
 
 
 def obtain_probabilities(input_text: str, local: bool):
@@ -127,14 +133,19 @@ def main():
     parser.add_argument("--train_csv", type=str, help="CSV file with training data.")
     parser.add_argument("--test_csv", type=str, help="CSV file with testing data.")
     parser.add_argument("--input_text", type=str, help="Input text, for probability calculation.")
-    parser.add_argument("--train", action="store_true", help="Start fine-tuning the pre-trained transformer model.")
+    parser.add_argument("--train", action="store_true", help="Start fine-tuning the pre-trained transformer model and "
+                                                             "sending it to the hub.")
+    parser.add_argument("--trainlocal", action="store_true", help="Start fine-tuning the pre-trained transformer model"
+                                                                  " and saving it locally.")
     parser.add_argument("--pred", action="store_true", help="Calculate the probability for helping given a text"
                                                             " using a remote model.")
     parser.add_argument("--predlocal", action="store_true", help="Calculate the probability for helping given a text"
                                                                  " using a local model.")
     arguments = parser.parse_args()
     if arguments.train:
-        start_training(arguments.train_csv, arguments.test_csv)
+        start_training(arguments.train_csv, arguments.test_csv, local=False)
+    elif arguments.trainlocal:
+        start_training(arguments.train_csv, arguments.test_csv, local=True)
     elif arguments.pred:
         obtain_probabilities(arguments.input_text, local=False)
     elif arguments.predlocal:
