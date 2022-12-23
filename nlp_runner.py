abeee5f1b001919ac51aef5645a790448bb00370
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2022-12-23 14:00:35 +0000

    Training the sanity check, on the GPU over a single instance.

--- nlp_runner.py
+++ nlp_runner.py
@@ -41,6 +41,9 @@ def load_and_split(data_file: str, test_size: float = 0.5) -> Tuple[str, str]:
     testing_csv_file: str = "data/testing_data.csv"
 
     training_dataframe, testing_dataframe = train_test_split(original_dataframe, test_size=test_size)
+    logging.info(
+        f"{len(training_dataframe.index)} training instances, {len(testing_dataframe.index)} testing instances")
+
     training_dataframe.to_csv(training_csv_file, index=False)
     testing_dataframe.to_csv(testing_csv_file, index=False)
 
@@ -51,16 +54,16 @@ def train(data_file: str = "data/survivor_responses.csv"):
     training_csv_file, testing_csv_file = load_and_split(data_file)
 
     type_analyser: TransformerTypeAnalyser = TransformerTypeAnalyser()
-    type_analyser.train(training_csv_file, testing_csv_file)
+    type_analyser.train_from_files(training_csv_file, testing_csv_file)
 
 
 def main():
     # train()
 
     target_data_file: str = "data/survivor_responses.csv"
-    preprocess_data(original_data_file="data/final data_agreed.xlsx",
-                    target_data_file=target_data_file)
-    load_and_split(data_file=target_data_file)
+    # preprocess_data(original_data_file="data/final data_agreed.xlsx",
+    #                 target_data_file=target_data_file)
+    load_and_split(data_file=target_data_file, test_size=0.3)
     # predict("Sure! I'll help")
     # predict("Sorry, I can't")
 
--- transformer_analyser.py
+++ transformer_analyser.py
@@ -1,6 +1,4 @@
-import os
-
-from torch import Tensor
+import datetime
 
 SEED: int = 42
 import numpy as np
@@ -10,45 +8,39 @@ np.random.seed(SEED)
 import tensorflow as tf
 
 tf.random.set_seed(SEED)
-# physical_devices = tf.config.list_physical_devices("GPU")
-# for device in physical_devices:
-#     tf.config.experimental.set_memory_growth(device, True)
-# config = tf.compat.v1.ConfigProto()
-# config.gpu_options.allow_growth = True
-# session = tf.compat.v1.Session(config=config)
-os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-
 
 import logging
 from argparse import ArgumentParser
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Any
 
 import pandas as pd
 from datasets import Dataset, DatasetDict
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras import Model
+from keras.callbacks import EarlyStopping, TensorBoard, Callback
+from keras import Model
 from transformers import AutoTokenizer, BertTokenizerFast, TFAutoModelForSequenceClassification, \
-    DataCollatorWithPadding, pipeline, Pipeline
+    pipeline, Pipeline
 
 GROUP_IDENTITY_CLASS: str = "LABEL_1"
 TEXT_CONTENT_COLUMN = "text"
-TEXT_LABEL_COLUMN = "label"
-NON_ESSENTIAL_COLUMNS = ["title", "URL", "starting_time", "context", "logged_by", "Observation"]
+TEXT_LABEL_COLUMN = "will_help"
+NON_ESSENTIAL_COLUMNS = ["title", "URL", "starting_time", "context", "logged_by"]
 
 
 class TransformerTypeAnalyser(object):
 
-    def __init__(self):
+    def __init__(self, epochs=65):
         self.model_checkpoint: str = "bert-base-uncased"
         self.hub_model_id: str = "cptanalatriste/request-for-help"
         self.pipeline_task: str = "text-classification"
         self.output_directory: str = "./model"
+        self.log_directory: str = "logs/"
+
         self.model_input_names: List[str] = ['input_ids', 'token_type_ids', 'attention_mask']
 
         self.num_labels: int = 2
         self.batch_size: int = 32
         self.learning_rate: float = 3e-5
-        self.epochs = 65
+        self.epochs = epochs
         self.early_stopping_patience: int = int(self.epochs / 3)
 
         self.tokenizer: Optional[BertTokenizerFast] = None
@@ -57,18 +49,11 @@ class TransformerTypeAnalyser(object):
     def tokenize(self, data_batch):
         return self.tokenizer(data_batch[TEXT_CONTENT_COLUMN], truncation=True, padding="max_length")
 
-    @staticmethod
-    def convert_csv_to_dataset(csv_file: str) -> Dataset:
-        dataframe: pd.DataFrame = pd.read_csv(csv_file)
-        dataset: Dataset = Dataset.from_pandas(dataframe)
-
-        return dataset
-
     def convert_dataset_to_tensorflow(self, encoded_datasets: Dataset, shuffle: bool) -> tf.data.Dataset:
         tensorflow_dataset: Dataset = encoded_datasets.remove_columns([TEXT_CONTENT_COLUMN]).with_format("tensorflow")
 
-        features: Dict[str, Tensor] = {input_name: tensorflow_dataset[input_name]
-                                       for input_name in self.model_input_names}
+        features: Dict[str, Any] = {input_name: tensorflow_dataset[input_name]
+                                    for input_name in self.model_input_names}
         result_dataset: tf.data.Dataset = tf.data.Dataset.from_tensor_slices(
             (features, tensorflow_dataset[TEXT_LABEL_COLUMN]))
         if shuffle:
@@ -78,47 +63,48 @@ class TransformerTypeAnalyser(object):
 
         return result_dataset
 
-    def train(self, training_data_file: str, testing_data_file: str, local=False):
+    def train_from_files(self, training_data_file: str, testing_data_file: Optional[str] = None):
+        self.train(pd.read_csv(training_data_file), pd.read_csv(testing_data_file))
+
+    def train(self, training_data: pd.DataFrame, testing_data: Optional[pd.DataFrame] = None):
+        logging.info("Starting training...")
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_checkpoint)
         self.model = TFAutoModelForSequenceClassification.from_pretrained(self.model_checkpoint,
                                                                           num_labels=self.num_labels)
         self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate),
                            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                            metrics=tf.metrics.SparseCategoricalAccuracy())
+        logging.info("Model compiled...")
 
-        dataset_dict: DatasetDict = DatasetDict({
-            "train": self.convert_csv_to_dataset(training_data_file),
-            "test": self.convert_csv_to_dataset(testing_data_file)
-        })
+        dataset_dict_input: Dict[str, Dataset] = {
+            "train": Dataset.from_pandas(training_data)
+        }
+        if testing_data:
+            dataset_dict_input["test"] = Dataset.from_pandas(testing_data)
+        dataset_dict: DatasetDict = DatasetDict(dataset_dict_input)
 
         logging.info("Encoding text ...")
         encoded_datasets: DatasetDict = dataset_dict.map(self.tokenize, batched=True)
         encoded_datasets = encoded_datasets.remove_columns(NON_ESSENTIAL_COLUMNS)
 
         training_dataset: tf.data.Dataset = self.convert_dataset_to_tensorflow(encoded_datasets["train"], shuffle=True)
-        testing_dataset: tf.data.Dataset = self.convert_dataset_to_tensorflow(encoded_datasets["test"], shuffle=False)
+        testing_dataset: Optional[tf.data.Dataset] = None
 
         logging.info(f"Encoding finished!")
+        training_callbacks: List[Callback] = [TensorBoard(log_dir=get_log_directory(self.log_directory))]
 
-        early_stopping_callback: EarlyStopping = EarlyStopping(monitor='val_loss',
-                                                               patience=self.early_stopping_patience,
-                                                               verbose=True,
-                                                               restore_best_weights=True)
+        if "test" in encoded_datasets:
+            testing_dataset = self.convert_dataset_to_tensorflow(encoded_datasets["test"], shuffle=False)
+            training_callbacks.append(EarlyStopping(monitor='val_loss',
+                                                    patience=self.early_stopping_patience,
+                                                    verbose=True,
+                                                    restore_best_weights=True))
 
-        if local:
-            self.model.fit(training_dataset, validation_data=testing_dataset, epochs=self.epochs,
-                           callbacks=[early_stopping_callback])
-            self.model.save_pretrained(self.output_directory)
-        else:
-            # push_to_hub_callback: PushToHubCallback = PushToHubCallback(output_dir=self.output_directory,
-            #                                                             tokenizer=self.tokenizer,
-            #                                                             hub_model_id=self.hub_model_id)
-            #
-            # self.model.fit(training_dataset, validation_data=testing_dataset, epochs=self.epochs,
-            #                callbacks=[early_stopping_callback, push_to_hub_callback])
-            #
-            # logging.info(f"Training finished! Model is available at the hub with id {self.hub_model_id}")
-            pass
+        self.model.fit(training_dataset,
+                       validation_data=testing_dataset,
+                       epochs=self.epochs,
+                       callbacks=training_callbacks)
+        self.model.save_pretrained(self.output_directory)
 
         self.tokenizer.save_pretrained(self.output_directory)
         logging.info(f"Model and Tokenizer saved at {self.output_directory}")
@@ -141,13 +127,17 @@ class TransformerTypeAnalyser(object):
         return group_identity_probability
 
 
-def start_training(training_csv: str, testing_csv_file: str, local: bool) -> None:
+def get_log_directory(base_directory: str = "logs/") -> str:
+    return base_directory + datetime.datetime.now().strftime("run_%Y_%m_%d_%H_%M_%S")
+
+
+def start_training(training_csv: str, testing_csv_file: str) -> None:
     logging.basicConfig(level=logging.INFO)
     logging.info(f"training_csv {training_csv}")
     logging.info(f"testing_csv_file {testing_csv_file}")
 
     type_analyser: TransformerTypeAnalyser = TransformerTypeAnalyser()
-    type_analyser.train(training_csv, testing_csv_file, local)
+    type_analyser.train_from_files(training_csv, testing_csv_file)
 
 
 def obtain_probabilities(input_text: str, local: bool):
@@ -173,9 +163,9 @@ def main():
                                                                  " using a local model.")
     arguments = parser.parse_args()
     if arguments.train:
-        start_training(arguments.train_csv, arguments.test_csv, local=False)
+        start_training(arguments.train_csv, arguments.test_csv)
     elif arguments.trainlocal:
-        start_training(arguments.train_csv, arguments.test_csv, local=True)
+        start_training(arguments.train_csv, arguments.test_csv)
     elif arguments.pred:
         obtain_probabilities(arguments.input_text, local=False)
     elif arguments.predlocal:
