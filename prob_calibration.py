fd1503afd8debc8f4e74680db218b2aff3aadf85
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2022-12-29 11:49:50 +0000

    Calibrating with Isotonic Regression. Only a minor improved shown

--- prob_calibration.py
+++ prob_calibration.py
@@ -1,8 +1,34 @@
-from typing import List
+import logging
+from typing import List, Optional
 
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.calibration import calibration_curve
+from sklearn.isotonic import IsotonicRegression
+
+from transformer_analyser import TransformerTypeAnalyser
+
+
+class CalibratedTypeEstimator:
+
+    def __init__(self, original_type_estimator: TransformerTypeAnalyser):
+        self.original_type_estimator: TransformerTypeAnalyser = original_type_estimator
+        self.isotonic_calibrator: Optional[IsotonicRegression] = None
+
+    def calibrate(self, expressions: List[str], true_labels: List[int], number_of_bins: int) -> None:
+        positive_probabilities: List[float] = self.original_type_estimator.obtain_probabilities(expressions,
+                                                                                                local=True)
+        bin_true_probabilities, bin_predicted_probabilities = calibration_curve(true_labels, positive_probabilities,
+                                                                                n_bins=number_of_bins)
+
+        self.isotonic_calibrator = IsotonicRegression(out_of_bounds="clip")
+        self.isotonic_calibrator.fit(bin_predicted_probabilities, bin_true_probabilities)
+        logging.info("Isotonic Regression fitted.")
+
+    def obtain_probabilities(self, expressions: List[str]) -> List[float]:
+        original_probabilities: List[float] = self.original_type_estimator.obtain_probabilities(expressions,
+                                                                                                local=True)
+        return self.isotonic_calibrator.predict(original_probabilities)
 
 
 def plot_reliability_diagram(true_labels: List[int], positive_probabilities: List[float], number_of_bins: int):
